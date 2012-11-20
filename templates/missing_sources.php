66ac34c6a873088ad6c13efcb9268b1215baacdd
webzunft <post@webzunft.de>
2012-11-20 23:10:34 +0100

    removed some comments

--- templates/missing_sources.php
+++ templates/missing_sources.php
@@ -32,19 +32,3 @@ if (!empty($attachments)) {
     }
     ?></tbody></table><?php
 }
-
-/**
- * aim: list all posts where one or another image source is missing
- * 1. get all attachments that do not have the isc_image_source or isc_image_source_own meta field
- * problem: attachments without this value are not queried at all
- * 3 solutions:
- * a) build custom query with $wpdb
- * b) query all attachments and than loop through them one by one (not recommented for speed reasons)
- * planen: create image_source-fields for all attachments on install
- * 
- * 2. get all posts the attachments are attached to
- * 3. list them and link to them to easily fix it
- * list also unpublished posts?
- * also link to the image in the mediathek to change it directly there!
- * how to automatically create the meta fields for all attachments? => needed for the query
- */
