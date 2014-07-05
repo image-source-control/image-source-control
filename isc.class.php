4cb5b0089d9616c1c759928f491dfde43ecf72ea
Thomas Maier <post@webzunft.de>
2014-07-05 15:08:01 +0200

    moved missing image source message to dashboard only

--- isc.class.php
+++ isc.class.php
@@ -100,7 +100,7 @@ if (!class_exists('ISC_CLASS')) {
                 add_filter('attachment_fields_to_edit', array($this, 'add_isc_fields'), 10, 2);
                 add_filter('attachment_fields_to_save', array($this, 'isc_fields_save'), 10, 2);
 
-                add_action('admin_notices', array($this, 'admin_notices'));
+                add_action('admin_head-index.php', array($this, 'admin_notices'));
 
                 add_action('admin_menu', array($this, 'create_menu'));
                 add_action('admin_init', array($this, 'SAPI_init'));
@@ -500,7 +500,7 @@ if (!class_exists('ISC_CLASS')) {
                         'value' => '',
                         'compare' => '=',
                     ),
-                    // and image source is not set
+                    // and does not belong to an author
                     array(
                         'key' => 'isc_image_source_own',
                         'value' => '1',
@@ -1641,6 +1641,8 @@ if (!class_exists('ISC_CLASS')) {
         */
         public function admin_notices()
         {
+
+            // attachments without sources
             $args = array(
                 'post_type' => 'attachment',
                 'numberposts' => -1,
@@ -1654,17 +1656,33 @@ if (!class_exists('ISC_CLASS')) {
                     ),
                     array(
                         'key' => 'isc_image_source_own',
-                        'value' => '',
-                        'compare' => ''
+                        'value' => '1',
+                        'compare' => '!=',
                     )
                 )
             );
             $attachments = get_posts($args);
+
+            // load unindexed attachments
+            $args = array(
+                'post_type' => 'attachment',
+                'numberposts' => -1,
+                'post_status' => null,
+                'post_parent' => null,
+                'meta_query' => array(
+                    array(
+                        'key' => 'isc_image_source',
+                        'value' => 'any',
+                        'compare' => 'NOT EXISTS'
+                    ),
+                )
+            );
+            $attachments2 = get_posts($args);
             $options = $this->get_isc_options();
-            if (!empty($attachments) && $options['warning_onesource_missing'] ) {
+            if ((!empty($attachments) || !empty($attachments2)) && $options['warning_onesource_missing'] ) {
             $missing_src = esc_url(admin_url('upload.php?page=isc_missing_sources_page'));
             ?>
-                <div class="updated"><p><?php printf(__('One or more attachments still have no source. See the <a href="%s">missing sources</a> list', ISCTEXTDOMAIN), $missing_src);?></p></div>
+                <div class="error"><p><?php printf(__('One or more attachments still have no source. See the <a href="%s">missing sources</a> list', ISCTEXTDOMAIN), $missing_src);?></p></div>
             <?php
             }
         }
