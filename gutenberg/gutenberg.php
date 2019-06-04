a557e0bb7472472af6d622c981fc9714f2e810c9
Thomas Maier <post@webzunft.de>
2019-06-04 15:37:32 +0200

    moved image source settings to image block

---
+++ gutenberg/gutenberg.php
@@ -0,0 +1,98 @@
+<?php
+
+function isc_licences_text_to_array($licences = '') {
+  if($licences == '') return false;
+  // split the text by line
+  $licences_array = preg_split('/\r?\n/', trim($licences));
+  if(count($licences_array) == 0 ) return false;
+  // create the array with licence => url
+  $new_licences = array();
+  foreach($licences_array as $_licence) {
+      if(trim($_licence) != '') {
+          $temp = explode('|', $_licence);
+          $new_licences[$temp[0]] = array();
+          if( isset($temp[1]))
+              $new_licences[$temp[0]]['url'] = esc_url ($temp[1]);
+      }
+  }
+
+  if($new_licences == array()) return false;
+  else return $new_licences;
+}
+
+function isc_addSourceToCoreImageBlock() {
+  wp_enqueue_script(
+    'source-to-core-image-block',
+    plugin_dir_url(__FILE__) . 'source-to-core-image-block.js',
+    array('wp-api', 'lodash', 'wp-blocks', 'wp-editor', 'wp-element', 'wp-i18n',),
+    true
+  );
+
+  //Reading the options and making them avaialable for our Gutenberg block
+  $options = get_option('isc_options');
+  $licenses_js_array = array();
+  if($options['enable_licences'] && $licences = isc_licences_text_to_array($options['licences'])) {
+    $licenses_js_array[] = array('value' => '', 'label' => '--');
+    foreach($licences as $_licence_name => $_licence_data) {
+      $licenses_js_array[] = array('value' => $_licence_name, 'label' => $_licence_name);
+    }
+  }
+  wp_localize_script('source-to-core-image-block', 'isc_options', $licenses_js_array );
+}
+
+//Making our custom meta fields available to wp rest api.
+function isc_register_api_fields() {
+ register_rest_field( 'attachment', 'isc_image_source',
+    array(
+      'get_callback'    => 'isc_get_attachment_meta',
+      'update_callback' => 'slug_update_attachment_meta',
+      'schema'          => null,
+      'auth_callback' => function(){
+        return current_user_can('edit_posts');
+       }
+    )
+ );
+ register_rest_field( 'attachment', 'isc_image_source_own',
+    array(
+       'get_callback'    => 'isc_get_attachment_meta',
+       'update_callback' => 'slug_update_attachment_meta',
+       'schema'          => null,
+       'auth_callback' => function(){
+         return current_user_can('edit_posts');
+        }
+    )
+ );
+ register_rest_field( 'attachment', 'isc_image_source_url',
+    array(
+       'get_callback'    => 'isc_get_attachment_meta',
+       'update_callback' => 'slug_update_attachment_meta',
+       'schema'          => null,
+       'auth_callback' => function(){
+         return current_user_can('edit_posts');
+        }
+    )
+ );
+ register_rest_field( 'attachment', 'isc_image_licence',
+    array(
+       'get_callback'    => 'isc_get_attachment_meta',
+       'update_callback' => 'slug_update_attachment_meta',
+       'schema'          => null,
+       'auth_callback' => function(){
+         return current_user_can('edit_posts');
+        }
+    )
+ );
+}
+
+//Function to get a meta field, otherwise it will return a null value
+function isc_get_attachment_meta($object, $field_name, $request) {
+  return get_post_meta($object['id'], $field_name, true);
+}
+//Function to get a meta field, otherwise it becomes read only.
+function slug_update_attachment_meta($value, $object, $field_name ) {
+  return update_post_meta($object->ID, $field_name, $value);
+}
+
+add_action('init', function() { wp_enqueue_script( 'wp-api' ); });
+add_action('rest_api_init', 'isc_register_api_fields');
+add_action('enqueue_block_editor_assets', 'isc_addSourceToCoreImageBlock');
---
+++ gutenberg/source-to-core-image-block.js
@@ -0,0 +1,245 @@
+"use strict";
+
+var assign = lodash.assign;
+var addFilter = wp.hooks.addFilter;
+var __ = wp.i18n.__;
+var createHigherOrderComponent = wp.compose.createHigherOrderComponent;
+var Fragment = wp.element.Fragment;
+var InspectorControls = wp.editor.InspectorControls;
+var PanelBody = wp.components.PanelBody;
+var SelectControl = wp.components.SelectControl;
+var TextControl = wp.components.TextControl;
+var CheckboxControl = wp.components.CheckboxControl;
+var isc_loading_text = 'Loading...';
+
+var isc_enableSourceControlOnBlocks = [
+    'core/image',
+];
+
+
+var isc_refresh = [];
+var isc_forceRefresh = function(id) {
+  if(isc_refresh.indexOf(id) === -1) {
+    isc_refresh.push(id);
+  }
+}
+var isc_disableRefresh = function(id) {
+  var index = isc_refresh.indexOf(id);
+  isc_refresh.splice(index, 1);
+}
+
+var isc_update_meta_field = function(id, field, value, setAttributes) {
+  var opts = {};
+  opts[field] = value;
+  fetch( '/wp-json/wp/v2/media/'+id , {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      'X-WP-Nonce': wpApiSettings.nonce
+    },
+    body: JSON.stringify(opts)
+  })
+  .then(res=>{ setAttributes({isc_image_source_fetching: false}); })
+  .catch(function() {
+    setAttributes({isc_image_source_fetching: false});
+    //TODO: Handle errors.
+  });
+}
+
+var isc_addSourceControlAttribute = function( settings, name ) {
+  if ( ! isc_enableSourceControlOnBlocks.includes( name ) ) {
+      return settings;
+  }
+
+  // Use Lodash's assign to gracefully handle if attributes are undefined
+  settings.attributes = assign( settings.attributes, {
+		isc_image_source: {
+			type: 'string',
+			default: '',
+		},
+    isc_image_source_own: {
+			type: 'boolean',
+			default: false,
+		},
+    isc_image_source_url: {
+      type: 'string',
+      default: '',
+    },
+    isc_image_licence: {
+      type: 'string',
+      default: '',
+    },
+    isc_image_source_done: {
+			type: 'boolean',
+			default: true,
+		},
+    isc_image_source_must_refresh: {
+			type: 'boolean',
+			default: true,
+		},
+    isc_image_source_fetching: {
+			type: 'boolean',
+			default: false,
+		}
+	} );
+
+  return settings;
+};
+
+addFilter( 'blocks.registerBlockType', 'image-source-control/attributes', isc_addSourceControlAttribute );
+
+var isc_withSourceControl = createHigherOrderComponent(function (BlockEdit) {
+  return function (props) {
+    // Do nothing if it's another block than our defined ones.
+    if (!isc_enableSourceControlOnBlocks.includes(props.name)) {
+      return React.createElement(BlockEdit, props);
+    }
+
+    //If an image has not been selected yet, do not display the source control fields.
+    if(isNaN(props.attributes.id)) {
+      return React.createElement(BlockEdit, props);
+    }
+
+    props.setAttributes({
+      isc_image_source_must_refresh: (isc_refresh.indexOf(props.attributes.id) > -1) ? true : false
+    });
+
+    var isc_image_source = props.attributes.isc_image_source;
+    var isc_image_source_own = props.attributes.isc_image_source_own;
+    var isc_image_source_url = props.attributes.isc_image_source_url;
+    var isc_image_licence = props.attributes.isc_image_licence;
+    var id = props.attributes.id;
+    var isc_image_source_done = props.attributes.isc_image_source_done;
+    var isc_image_source_must_refresh = props.attributes.isc_image_source_must_refresh;
+    var isc_image_source_fetching = props.attributes.isc_image_source_fetching;
+
+    //Add source control class to the node, in case a source is set.
+    if (isc_image_source && isc_image_source.trim() !== '' && isc_image_source.trim() !== isc_loading_text) {
+      props.attributes.className = "has-source-control";
+    }
+
+    //If the data has not been fetched, fetch it using the endpoints we defined in gutenberg.php
+    if(isc_image_source_done && isc_image_source_must_refresh && !isc_image_source_fetching) {
+      props.setAttributes({
+        isc_image_source_fetching: true
+      });
+      fetch('/wp-json/wp/v2/media/'+id, {
+        headers: {
+          'X-WP-Nonce': wpApiSettings.nonce
+        }
+      })
+      .then((response) => response.json())
+      .then(function(data) {
+        props.setAttributes({
+          isc_image_source: data.isc_image_source,
+          isc_image_source_own: (data.isc_image_source_own == '1') ? true : false,
+          isc_image_source_url: data.isc_image_source_url,
+          isc_image_licence: data.isc_image_licence,
+          isc_image_source_must_refresh: false,
+          isc_image_source_fetching: false
+        });
+        isc_disableRefresh(id);
+      })
+      .catch(function() {
+        props.setAttributes({
+          isc_image_source_fetching: false
+        });
+        //TODO: Handle errors.
+      });
+    }
+
+    //This is using native backbone client, however, it does not support catching errors:
+    /*
+    if(isc_image_source_done) {
+      wp.api.loadPromise.done( function() {
+        window.media = new wp.api.models.Media({id: id });
+        window.media.fetch()
+        .then(function(response) {
+          props.setAttributes({
+            isc_image_source: response.isc_image_source,
+            isc_image_source_own: (response.isc_image_source_own == '1') ? true : false,
+            isc_image_source_url: response.isc_image_source_url
+          });
+        });
+      });
+    }
+    */
+
+    BlockEdit.onClick = isc_forceRefresh(id);
+
+    var enable_licences = (isc_options.length > 0) ? {} : { display: 'none' };
+    var isFetching = (isc_image_source_fetching === true) ? true : null;
+    //Extends the block by adding the source control fields.
+    return React.createElement(Fragment, null, React.createElement(BlockEdit, props), React.createElement(InspectorControls, null, React.createElement(PanelBody, {
+      title: __('My Source Control'),
+      initialOpen: true,
+    }, React.createElement(TextControl, {
+      label: __('Image Source'),
+      disabled: isFetching,
+      value: isc_image_source,
+      placeholder: 'Some explanation text...',
+      onChange: function onChange(isc_image_source) {
+        props.setAttributes({
+          isc_image_source: isc_image_source,
+          isc_image_source_done: false
+        });
+      },
+      onBlur: (e) => {
+        var text = e.target.value;
+        isc_update_meta_field(id, 'isc_image_source', text, props.setAttributes);
+        props.setAttributes({
+          isc_image_source_done: true,
+          isc_image_source_fetching: true
+        });
+      }
+    }), React.createElement(CheckboxControl, {
+      label: "This is my image",
+      disabled: isFetching,
+      checked: isc_image_source_own,
+      onChange: function onChange(isc_image_source_own) {
+        props.setAttributes({
+          isc_image_source_own: isc_image_source_own,
+          isc_image_source_fetching: true
+        });
+        isc_update_meta_field(id, 'isc_image_source_own', isc_image_source_own | 0, props.setAttributes);
+      }
+    }), React.createElement(TextControl, {
+      label: __('Image Source URL'),
+      disabled: isFetching,
+      value: isc_image_source_url,
+      placeholder: 'Some explanation text...',
+      onChange: function onChange(isc_image_source_url) {
+        props.setAttributes({
+          isc_image_source_url: isc_image_source_url,
+          isc_image_source_done: false
+        });
+      },
+      onBlur: (e) => {
+        var text = e.target.value;
+        isc_update_meta_field(id, 'isc_image_source_url', text, props.setAttributes);
+        props.setAttributes({
+          isc_image_source_done: true,
+          isc_image_source_fetching: true
+        });
+      }
+    }),
+    React.createElement(SelectControl, {
+      label: __('Image License'),
+      disabled: isFetching,
+      value: isc_image_licence,
+      onChange: function onChange(isc_image_licence) {
+        isc_update_meta_field(id, 'isc_image_licence', isc_image_licence, props.setAttributes);
+        props.setAttributes({
+          isc_image_licence: isc_image_licence,
+          isc_image_source_fetching: true
+        });
+      },
+      options: isc_options,
+      style: enable_licences
+    })
+  )));
+  };
+}, 'isc_withSourceControl');
+
+addFilter( 'editor.BlockEdit', 'image-source-control/editor', isc_withSourceControl );
