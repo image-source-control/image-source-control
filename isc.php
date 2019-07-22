b21fc136c50cf3586394a2e5bbd91881d07bddb2
Thomas Maier <post@webzunft.de>
2019-07-22 18:42:08 +0200

    version

--- isc.php
+++ isc.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Plugin Name: Image Source Control
-  Version: 1.10
+  Version: 1.10.1
   Plugin URI: https://webgilde.com/en/image-source-control/
   Description: The Image Source Control saves the source of an image, lists them and warns if it is missing.
   Author: Thomas Maier
@@ -35,7 +35,7 @@ if ( ! function_exists( 'add_action' ) ) {
     exit();
 }
 
-define( 'ISCVERSION', '1.10' );
+define( 'ISCVERSION', '1.10.1' );
 define( 'ISCNAME', 'Image Source Control' );
 define( 'ISCDIR', basename( dirname( __FILE__ ) ) );
 define( 'ISCPATH', plugin_dir_path( __FILE__ ) );
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: webzunft
 Tags: image, images, picture, picture source, image source, mediathek, media, caption, copyright
 Requires at least: 5.0
 Tested up to: 5.2
-Stable tag: 1.10
+Stable tag: 1.10.1
 Requires PHP: 5.6
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -102,7 +102,7 @@ See the _Instructions_ section [here](https://wordpress.org/plugins/image-source
 
 == Changelog ==
 
-= untagged =
+= 1.10.1 =
 
 * fix for block translations
 
