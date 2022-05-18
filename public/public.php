8351a5b8777f674b0f811cf233e4067757c8ecb9
Thomas Maier <post@webzunft.de>
2022-05-18 09:31:46 +0200

    set link color in overlay
    The links in overlays were often overridden by the theme font color, which are often dark. So it was hard to read on the dark background. This PR sets a static color (white) by default. Users can still override it with custom CSS.
    
    Fixes #178

--- public/public.php
+++ public/public.php
@@ -104,9 +104,9 @@ class ISC_Public extends ISC_Class {
 				.isc-source { position: relative; display: inline-block; }
                 .wp-block-cover .isc-source { position: static; }
 				<?php
-				// The 2022 theme adds display:block to the featured image block, which creates additional line breaks. The following line fixes that.
+				// The 2022 theme adds display:block to the featured image block, which creates additional line breaks. `display: inline-block` fixes that.
 				?>
-				.isc-source-text a { display: inline-block; }
+				.isc-source-text a { display: inline-block; color: #fff; }
 			</style>
 			<?php
 	}
