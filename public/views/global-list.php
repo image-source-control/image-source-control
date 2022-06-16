cabcbbfc2aae0a855cb6f4ad277413c6af3d87ae
Thomas Maier <post@webzunft.de>
2022-06-16 14:16:49 +0200

    move global list HTML to a view file

---
+++ public/views/global-list.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Render the global list of image sources
+ * @var array $options plugin options.
+ * @var array $atts attachment information.
+ *
+ * Added comment `isc_stop_overlay` as a class to the table to suppress overlays within it starting at that point
+ * todo: allow overlays to start again after the table
+ **/
+?>
+<div class="isc_all_image_list_box isc_stop_overlay" style="overflow: scroll;">
+	<table>
+		<thead>
+		<?php if ( $options['thumbnail_in_list'] ) : ?>
+			<th><?php esc_html_e( 'Thumbnail', 'image-source-control-isc' ); ?></th>
+		<?php endif; ?>
+		<th><?php esc_html_e( 'Attachment’s ID', 'image-source-control-isc' ); ?></th>
+		<th><?php esc_html_e( 'Title', 'image-source-control-isc' ); ?></th>
+		<th><?php esc_html_e( 'Attached to', 'image-source-control-isc' ); ?></th>
+		<th><?php esc_html_e( 'Source', 'image-source-control-isc' ); ?></th>
+		</thead>
+		<tbody>
+		<?php foreach ( $atts as $id => $data ) : ?>
+			<?php
+			$source = $this->render_image_source_string( $id );
+			?>
+			<tr>
+				<?php
+				$v_align = '';
+				if ( $options['thumbnail_in_list'] ) :
+					$v_align = 'style="vertical-align: top;"';
+					?>
+					<?php if ( 'custom' !== $options['thumbnail_size'] ) : ?>
+					<td><?php echo wp_get_attachment_image( $id, $options['thumbnail_size'] ); ?></td>
+				<?php else : ?>
+					<td><?php echo wp_get_attachment_image( $id, array( $options['thumbnail_width'], $options['thumbnail_height'] ) ); ?></td>
+				<?php endif; ?>
+				<?php endif; ?>
+				<td <?php echo $v_align; ?>><?php echo $id; ?></td>
+				<td <?php echo $v_align; ?>><?php echo $data['title']; ?></td>
+				<td <?php echo $v_align; ?>><?php echo $data['posts']; ?></td>
+				<td <?php echo $v_align; ?>><?php echo $source; ?></td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table></div>
