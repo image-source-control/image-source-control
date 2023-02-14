3046fbb6d894084082d686f5b81fb2ba7086e7e6
Thomas Maier <post@webzunft.de>
2023-02-14 16:44:28 +0100

    Limit checks for empty sources
    ISC checks all existing images for missing sources. This caused timeouts on sites with a lot of images.
    
    The change now just limits such queries to 100. Sites with more images would not benefit from longer lists anyway.

--- admin/templates/sources.php
+++ admin/templates/sources.php
@@ -10,6 +10,15 @@ $attachments = ISC_Model::get_attachments_with_empty_sources();
 if ( ! empty( $attachments ) ) :
 	?>
 	<h2><?php esc_html_e( 'Images without sources', 'image-source-control-isc' ); ?></h2>
+	<?php if( count( $attachments ) >= ISC_Model::MAX_POSTS ) : ?>
+		<p><?php
+			printf(
+				// translators: %d is the number of entries in the following table
+				esc_html__( 'The list only shows the last %d images.', 'image-source-control-isc' ),
+				ISC_Model::MAX_POSTS
+			); ?>
+		</p>
+	<?php endif; ?>
 	<table class="widefat isc-table" style="width: 80%;" >
 		<thead>
 			<tr>
@@ -49,6 +58,15 @@ if ( ! empty( $attachments ) ) :
 	<h2><?php esc_html_e( 'Images with unknown position', 'image-source-control-isc' ); ?></h2>
 	<p><?php esc_html_e( 'The list contains images that neither have sources nor were yet found by ISC on your site.', 'image-source-control-isc' ); ?>&nbsp;
 	<?php esc_html_e( 'They might not need a source after all.', 'image-source-control-isc' ); ?></p>
+	<?php if( count( $attachments ) >= ISC_Model::MAX_POSTS ) : ?>
+		<p><?php
+			printf(
+			// translators: %d is the number of entries in the following table
+				esc_html__( 'The list only shows the last %d images.', 'image-source-control-isc' ),
+				ISC_Model::MAX_POSTS
+			); ?>
+		</p>
+	<?php endif; ?>
 	<table class="widefat isc-table" style="width: 80%;" >
 		<thead>
 			<tr>
