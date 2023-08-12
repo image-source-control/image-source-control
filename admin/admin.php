image-source-control/image-source-control

commit 7e5bca0cdf26a948aa67e2c7b0f68851bb35fdf9

Fix PHP notice by unrelevant check
A PHP notice showed for a missing `HTTP_REFERER`. Looking at the code, I noticed that it was supposed to fix an issue, which we have fixed better in 760e60cbd045011e836f5de47a1878f5d5d2be81. The check was also unneeded now since we are no longer routing through `media.php` since 402d7aa15603c53cd96781281e82cf973260388b.

admin/admin.php
@@ -218,7 +218,7 @@ class ISC_Admin extends ISC_Class {
 	public function add_isc_fields( $form_fields, $post ) {
 		$options = $this->get_isc_options();
 
-		if ( strpos( sanitize_text_field( $_SERVER['HTTP_REFERER'] ), 'wp-admin/upload.php' ) === false && ( ! array_key_exists( 'block_options', $options ) || $options['block_options'] ) ) {
+		if ( ! array_key_exists( 'block_options', $options ) || $options['block_options'] ) {
 			return $form_fields;
 		}
 		if ( ! class_exists( 'ISC_Pro_Admin', false ) ) {

commit 657736c02f34dd35421eea959994a2896db8499f

Show the ISC fields more reliably
The changes in https://github.com/image-source-control/image-source-control/commit/7e5bca0cdf26a948aa67e2c7b0f68851bb35fdf9 caused the ISC fields to show in fewer places, when the Block Options support was enabled.

I reverted the change and adjusted it. Now, the ISC options should show on all media pages, except the media overlay when adding a new image to a post, while the Block Options are enabled. If they are disabled, the ISC fields also show in that media overlay.

admin/admin.php
@@ -207,10 +207,6 @@ class ISC_Admin extends ISC_Class {
 	/**
 	 * Add custom field to attachment
 	 *
-	 * @since 1.0
-	 * @updated 1.1
-	 * @updated 1.3.5 added field for license
-	 * @updated 1.5 added field for url
 	 * @param array  $form_fields field fields.
 	 * @param object $post post object.
 	 * @return array with form fields
@@ -218,9 +214,14 @@ class ISC_Admin extends ISC_Class {
 	public function add_isc_fields( $form_fields, $post ) {
 		$options = $this->get_isc_options();
 
-		if ( ! array_key_exists( 'block_options', $options ) || $options['block_options'] ) {
+		/**
+		 * Return, when the ISC fields are enabled for blocks and the HTTP_REFERER is a post edit page. This likely means, that the block editor is used.
+		 * While the URL to edit an attachment can also include "wp-admin.php.php", the HTTP_REFERER is then different
+		 */
+		if ( ! empty( $_SERVER['HTTP_REFERER'] ) && strpos( $_SERVER['HTTP_REFERER'], 'wp-admin/post.php' ) !== false && ( ! array_key_exists( 'block_options', $options ) || $options['block_options'] ) ) {
 			return $form_fields;
 		}
+
 		if ( ! class_exists( 'ISC_Pro_Admin', false ) ) {
 			$form_fields['isc_image_source_pro']['label'] = '';
 			$form_fields['isc_image_source_pro']['input'] = 'html';