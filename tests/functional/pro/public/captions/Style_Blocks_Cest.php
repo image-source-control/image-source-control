bd70a4585d57be7a3d9fd5f2a28749dae65cd7d9
Thomas Maier <post@webzunft.de>
2023-08-19 14:52:10 +0200

    Functional tests for image detection in styles
    The regular expression that looked for style blocks and URLs in them could be too generous and catch multiple style blocks. This was fixed in https://github.com/image-source-control/image-source-control-pro/pull/100/commits/00c78243a5478eb58f9c9936424a64cbf6095eb8. The new tests cover more scenarios in which multiple style blocks, style attributes, and `url()` information appear.

--- tests/functional/pro/public/captions/Style_Blocks_Cest.php
+++ tests/functional/pro/public/captions/Style_Blocks_Cest.php
@@ -30,6 +30,17 @@ class Style_Blocks_Cest {
 			'post_title'  => 'Image in style tag',
 			'guid'        => 'https://example.com/image-in-style-tag.jpg',
 		] );
+
+        // load image information
+        // image 2
+        $I->havePostmetaInDatabase(33, 'isc_image_source', 'iTeam');
+        $I->havePostmetaInDatabase(33, 'isc_image_source_url', 'https://www.iteam.com');
+        $I->havePostInDatabase([
+            'ID' => 33,
+            'post_author' => 1,
+            'post_title' => 'iTeam image',
+            'guid' => 'https://example.com/iteam.png',
+        ]);
 	}
 
 	public function test_no_image_found( \FunctionalTester $I ) {
@@ -56,4 +67,50 @@ class Style_Blocks_Cest {
 		// see the actual HTML output, including the data attribute and the overlay code following the STYLE tag
 		$I->seeInSource( '<style data-isc-source-text="Quelle: &lt;a href=&quot;https://www.imagesourcecontrol.com&quot; target=&quot;_blank&quot; rel=&quot;nofollow&quot;&gt;Team ISC&lt;/a&gt;">.some-element { background: url("https://example.com/image-in-style-tag.jpg"); }</style><span class="isc-source-text">Quelle: <a href="https://www.imagesourcecontrol.com" target="_blank" rel="nofollow">Team ISC</a></span>' );
 	}
+
+    /**
+     * Test multiple style tags with two creating a match
+     */
+    public function test_multiple_style_tags(\FunctionalTester $I)
+    {
+        $I->havePageInDatabase([
+            'post_name' => 'test-page',
+            'post_content' => <<<EOD
+<style id='style-with-id'>
+.some-irrelevant-element {
+	background: #32373c;
+	color: #ffffff;
+}
+</style>
+<style>
+.elementor-287 .elementor-element.elementor-element-2b65d67:not(.elementor-motion-effects-element-type-background), .elementor-287 .elementor-element.elementor-element-2b65d67 > .elementor-motion-effects-container > .elementor-motion-effects-layer{background-image:var(--e-bg-lazyload-loaded);--e-bg-lazyload:url("https://example.com/image-in-style-tag.jpg");}
+</style>
+<style id='some-other id'>
+.some-selector{background:url("https://example.com/iteam.png");}
+</style>
+EOD,
+        ]);
+
+        // Go to the page.
+        $I->amOnPage('/test-page');
+        // see the source information
+        $I->see('Team ISC');
+        $I->see('iTeam');
+        // see the actual HTML output, including the data attribute and the overlay code at the beginning of the DIV container
+        $I->seeInSource( <<<EOD
+<style id='style-with-id'>
+.some-irrelevant-element {
+	background: #32373c;
+	color: #ffffff;
+}
+</style>
+<style data-isc-source-text="Quelle: &lt;a href=&quot;https://www.imagesourcecontrol.com&quot; target=&quot;_blank&quot; rel=&quot;nofollow&quot;&gt;Team ISC&lt;/a&gt;">
+.elementor-287 .elementor-element.elementor-element-2b65d67:not(.elementor-motion-effects-element-type-background), .elementor-287 .elementor-element.elementor-element-2b65d67 > .elementor-motion-effects-container > .elementor-motion-effects-layer{background-image:var(--e-bg-lazyload-loaded);--e-bg-lazyload:url("https://example.com/image-in-style-tag.jpg");}
+</style><span class="isc-source-text">Quelle: <a href="https://www.imagesourcecontrol.com" target="_blank" rel="nofollow">Team ISC</a></span>
+<style data-isc-source-text="Quelle: &lt;a href=&quot;https://www.iteam.com&quot; target=&quot;_blank&quot; rel=&quot;nofollow&quot;&gt;iTeam&lt;/a&gt;" id='some-other id'>
+.some-selector{background:url("https://example.com/iteam.png");}
+</style><span class="isc-source-text">Quelle: <a href="https://www.iteam.com" target="_blank" rel="nofollow">iTeam</a></span>
+EOD
+        );
+    }
 }
