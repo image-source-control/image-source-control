image-source-control/image-source-control

commit 436ba99676426ca23b86cce7d4bba0f3f5e43e05

Introduce ISC\Includes\Standard_Source class
The new `Standard_Source` class contains all functions related to rendering the standard source.

Contains a new hook to manipulate the standard source and a few new functions as well.

This should eventually lead to leaner public classes.

admin/admin.php
@@ -246,7 +246,7 @@ class ISC_Admin extends ISC_Class {
 			sprintf(
 			// translators: %s is the name of an option
 				__( 'Currently selected: %s', 'image-source-control-isc' ),
-				ISC_Class::get_instance()->get_standard_source_label()
+				ISC\Includes\Standard_Source::get_standard_source_label()
 			);
 		$form_fields['isc_image_source_own']['html'] =
 			"<input type='checkbox' value='1' name='attachments[{$post->ID}][isc_image_source_own]' id='attachments[{$post->ID}][isc_image_source_own]' "
@@ -274,7 +274,7 @@ class ISC_Admin extends ISC_Class {
 			$form_fields['isc_image_licence']['html'] = $html;
 		}
 
-		return $form_fields;
+		return apply_filters( 'isc_admin_attachment_form_fields', $form_fields, $post, $options );
 	}
 
 	/**
@@ -700,9 +700,8 @@ class ISC_Admin extends ISC_Class {
 	 * Render options for standard image sources
 	 */
 	public function renderfield_standard_source() {
-		$options              = $this->get_isc_options();
-		$standard_source      = ! empty( $options['standard_source'] ) ? $options['standard_source'] : $this->get_standard_source();
-		$standard_source_text = $this->get_standard_source_text();
+		$standard_source      = ISC\Includes\Standard_Source::get_standard_source();
+		$standard_source_text = ISC\Includes\Standard_Source::get_standard_source_text();
 		require_once ISCPATH . '/admin/templates/settings/standard-source.php';
 
 		do_action( 'isc_admin_settings_template_after_standard_source' );
@@ -934,7 +933,7 @@ class ISC_Admin extends ISC_Class {
 		if ( isset( $input['by_author_text'] ) ) {
 			$output['standard_source_text'] = esc_html( $input['by_author_text'] );
 		} else {
-			$output['standard_source_text'] = isset( $input['standard_source_text'] ) ? esc_attr( $input['standard_source_text'] ) : $this->get_standard_source_text();
+			$output['standard_source_text'] = ISC\Includes\Standard_Source::get_standard_source_text();
 		}
 
 		/**

admin/templates/settings/standard-source.php
@@ -23,7 +23,7 @@
 	<input type="radio" name="isc_options[standard_source]" id="isc-custom-text-select" value="custom_text" <?php checked( $standard_source, 'custom_text' ); ?> />
 	<?php esc_html_e( 'Custom text', 'image-source-control-isc' ); ?>
 </label>
-<input type="text" id="isc-custom-text" name="isc_options[standard_source_text]" value="<?php echo esc_attr( $standard_source_text ); ?>" <?php disabled( $standard_source != 'custom_text' ); ?> class="regular-text" placeholder="<?php echo esc_attr( $this->get_standard_source_text() ); ?>"/>
+<input type="text" id="isc-custom-text" name="isc_options[standard_source_text]" value="<?php echo esc_attr( $standard_source_text ); ?>" <?php disabled( $standard_source != 'custom_text' ); ?> class="regular-text" placeholder="<?php echo esc_attr( ISC\Includes\Standard_Source::get_standard_source_text() ); ?>"/>
 	<?php do_action( 'isc_admin_settings_standard_source_options', $standard_source ); ?>
 </div>
 <br/>

includes/standard-source.php
@@ -1,0 +1,189 @@
+<?php
+/**
+ * Render the standard source
+ */
+namespace ISC\Includes;
+use ISC_Class;
+
+class Standard_Source {
+
+	/**
+	 * ISC options
+	 *
+	 * @var array
+	 */
+	protected static $options = null;
+
+	/**
+	 * Get the ISC options
+	 *
+	 * @return array
+	 */
+	protected static function get_options(): ?array {
+		if (self::$options === null) {
+			self::$options = ISC_Class::get_instance()->get_isc_options();
+		}
+		return self::$options;
+	}
+
+	/**
+	 * Get the standard source text as set up under Settings > Standard Source > Custom text
+	 * if there was no input, yet
+	 *
+	 * @return string
+	 */
+	public static function get_standard_source_text(): string {
+		$options = self::get_options();
+
+		if ( ! empty( $options['standard_source_text'] ) ) {
+			return $options['standard_source_text'];
+		} elseif ( isset( $options['by_author_text'] ) ) {
+			return $options['by_author_text'];
+		} else {
+			return sprintf( '© %s', get_home_url() );
+		}
+	}
+
+	/**
+	 * Return the standard source string for a given image if standard source is used for it
+	 *
+	 * @param int $attachment_id attachment ID.
+	 * @return string
+	 */
+	public static function get_standard_source_text_for_attachment( int $attachment_id ): string {
+
+		if ( ! self::use_standard_source( $attachment_id ) ) {
+			return '';
+		}
+
+		if ( self::standard_source_is( 'author_name' ) ) {
+			$author = get_post_field( 'post_author', $attachment_id );
+			$source = ! empty( $author ) ? get_the_author_meta( 'display_name', $author ) : '';
+		} else {
+			$source = self::get_standard_source_text();
+		}
+
+		/**
+		 * Filter the standard source text
+		 *
+		 * @param string $source standard source text.
+		 * @param int    $attachment_id attachment ID.
+		 */
+		return apply_filters( 'isc_standard_source_text_for_attachment', $source, $attachment_id );
+	}
+
+	/**
+	 * Verify the standard source option
+	 *
+	 * @param string $value value of the [standard_source] option.
+	 *
+	 * @return bool whether $value is identical to the standard source option or not.
+	 */
+	public static function standard_source_is( string $value ): bool {
+		$options = self::get_options();
+
+		if ( isset( $options['standard_source'] ) ) {
+			return $options['standard_source'] === $value;
+		}
+
+		/**
+		 * 2.0 moved the options to handle "own images" into "standard sources" and only offers a single choice for one of the options now
+		 * this section maps old to new settings
+		 */
+		if ( ! empty( $options['exclude_own_images'] ) ) {
+			return 'exclude' === $value;
+		} elseif ( ! empty( $options['use_authorname'] ) ) {
+			return 'author_name' === $value;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get the standard source setting
+	 *
+	 * @return string
+	 */
+	public static function get_standard_source() {
+		$options = self::get_options();
+
+		// options since 2.0
+		if ( ! empty( $options['standard_source'] ) ) {
+			return $options['standard_source'];
+		}
+
+		/**
+		 * 2.0 moved the options to handle "own images" into "standard sources" and only offers a single choice for one of the options now
+		 * this section maps old to new settings
+		 */
+		if ( ! empty( $options['exclude_own_images'] ) ) {
+			return 'exclude';
+		} elseif ( ! empty( $options['use_authorname'] ) ) {
+			return 'author_name';
+		} elseif ( ! empty( $options['by_author_text'] ) ) {
+			return 'custom_text';
+		}
+
+		return '';
+	}
+
+	/**
+	 * Get the label of the standard source label
+	 *
+	 * @param string|null $value optional value, if missing, will use the stored value.
+	 *
+	 * @return string
+	 */
+	public static function get_standard_source_label( string $value = null ) {
+		$labels = array(
+			'exclude'     => __( 'Exclude from lists', 'image-source-control-isc' ),
+			'author_name' => __( 'Author name', 'image-source-control-isc' ),
+			'custom_text' => __( 'Custom text', 'image-source-control-isc' ),
+			'iptc'        => __( 'IPTC meta data', 'image-source-control-isc' ),
+		);
+
+		if ( ! $value ) {
+			$value = self::get_standard_source();
+		}
+
+		return $labels[ $value ] ?? '';
+	}
+
+	/**
+	 * Check if the given attachment ought to use the standard source
+	 *
+	 * @param int $attachment_id attachment ID.
+	 *
+	 * @return bool true if standard source is used
+	 */
+	public static function use_standard_source( int $attachment_id ): bool {
+
+		$use_standard_source = (bool) apply_filters(
+			'isc_raw_attachment_use_standard_source',
+			get_post_meta( $attachment_id, 'isc_image_source_own', true ),
+			$attachment_id
+		);
+
+		// have another filter for frontend use for historic reasons
+		if ( ! is_admin() ) {
+			return (bool) apply_filters(
+				'isc_public_attachment_use_standard_source',
+				$use_standard_source,
+				$attachment_id
+			);
+		}
+
+		return $use_standard_source;
+	}
+
+	/**
+	 * Return true if the standard source is hidden for the given attachment
+	 * this is the case if the standard source is set to "exclude" and the attachment uses the standard source
+	 *
+	 * @param int $attachment_id attachment ID.
+	 * @return bool
+	 */
+	public static function hide_standard_source_for_image( int $attachment_id ): bool {
+		return self::standard_source_is( 'exclude' ) && self::use_standard_source( $attachment_id );
+	}
+}

isc.class.php
@@ -452,118 +452,6 @@ class ISC_Class {
 	}
 
 		/**
-		 * Get the standard source text as set up under Settings > Standard Source > Custom text
-		 * if there was no input, yet
-		 *
-		 * @return string
-		 */
-		public function get_standard_source_text() {
-			$options = $this->get_isc_options();
-			if ( ! empty( $options['standard_source_text'] ) ) {
-				return $options['standard_source_text'];
-			} elseif ( isset( $options['by_author_text'] ) ) {
-				return $options['by_author_text'];
-			} else {
-				return sprintf( '© %s', get_home_url() );
-			}
-		}
-
-		/**
-		 * Verify the standard source option
-		 *
-		 * @param string $value value of the [standard_source] option.
-		 * @return bool whether $value is identical to the standard source option or not.
-		 */
-		public function is_standard_source( $value ) {
-			$options = $this->get_isc_options();
-
-			if ( isset( $options['standard_source'] ) ) {
-				return $options['standard_source'] === $value;
-			}
-
-			/**
-			 * 2.0 moved the options to handle "own images" into "standard sources" and only offers a single choice for one of the options now
-			 * this section maps old to new settings
-			 */
-			if ( ! empty( $options['exclude_own_images'] ) ) {
-				return 'exclude' === $value;
-			} elseif ( ! empty( $options['use_authorname'] ) ) {
-				return 'author_name' === $value;
-			}
-
-			return false;
-		}
-
-
-		/**
-		 * Get the standard source setting
-		 *
-		 * @return string
-		 */
-		public function get_standard_source() {
-			$options = $this->get_isc_options();
-
-			// options since 2.0
-			if ( ! empty( $options['standard_source'] ) ) {
-				return $options['standard_source'];
-			}
-
-			/**
-			 * 2.0 moved the options to handle "own images" into "standard sources" and only offers a single choice for one of the options now
-			 * this section maps old to new settings
-			 */
-			if ( ! empty( $options['exclude_own_images'] ) ) {
-				return 'exclude';
-			} elseif ( ! empty( $options['use_authorname'] ) ) {
-				return 'author_name';
-			} elseif ( ! empty( $options['by_author_text'] ) ) {
-				return 'custom_text';
-			}
-
-			return false;
-		}
-
-		/**
-		 * Get the label of the standard source label
-		 *
-		 * @param string $value optional value, if missing, will use the stored value.
-		 * @return string
-		 */
-		public function get_standard_source_label( $value = null ) {
-			$options = $this->get_isc_options();
-
-			$labels = array(
-				'exclude'     => __( 'Exclude from lists', 'image-source-control-isc' ),
-				'author_name' => __( 'Author name', 'image-source-control-isc' ),
-				'custom_text' => __( 'Custom text', 'image-source-control-isc' ),
-			);
-
-			if ( ! $value ) {
-				$value = $this->get_standard_source();
-			}
-
-			if ( $value && isset( $labels[ $value ] ) ) {
-				return $labels[ $value ];
-			}
-
-			return false;
-		}
-
-		/**
-		 * Check if the given attachment ought to use the standard source
-		 *
-		 * @param int $attachment_id attachment ID.
-		 * @return bool true if standard source is used
-		 */
-		public static function use_standard_source( $attachment_id ) {
-			return (bool) apply_filters(
-				'isc_raw_attachment_use_standard_source',
-				get_post_meta( $attachment_id, 'isc_image_source_own', true ),
-				$attachment_id
-			);
-		}
-
-		/**
 		 * Get image source string before it was filtered for output
 		 *
 		 * @param int $attachment_id attachment ID.

isc.php
@@ -41,6 +41,7 @@ require_once ISCPATH . 'includes/model.php';
 require_once ISCPATH . 'includes/storage-model.php';
 require_once ISCPATH . 'includes/analyze-html.php';
 require_once ISCPATH . 'includes/log.php';
+require_once ISCPATH . 'includes/standard-source.php';
 
 if ( is_admin() ) {
 	if ( ! class_exists( 'ISC_Admin', false ) ) {

public/public.php
@@ -1,5 +1,8 @@
 <?php
-	/**
+
+use ISC\Includes\Standard_Source;
+
+/**
 	 * Handles all admin functionalities
 	 *
 	 * @todo move frontend-only functions from general class here
@@ -299,7 +302,7 @@ class ISC_Public extends ISC_Class {
 			}
 
 			// don’t show caption for own image if admin choose not to do so
-			if ( $this->is_standard_source( 'exclude' ) && self::use_standard_source( $id ) ) {
+			if ( Standard_Source::hide_standard_source_for_image( $id ) ) {
 				ISC_Log::log( sprintf( 'skipped "own" image for ID "%s"', $id ) );
 				continue;
 			}
@@ -460,13 +463,13 @@ class ISC_Public extends ISC_Class {
 		}
 
 		$attachments      = get_post_meta( $post_id, 'isc_post_images', true );
-		$exclude_standard = $this->is_standard_source( 'exclude' );
+		$exclude_standard = Standard_Source::standard_source_is( 'exclude' );
 
 		if ( ! empty( $attachments ) ) {
 			ISC_Log::log( sprintf( 'going through %d attachments', count( $attachments ) ) );
 			$atts = array();
 			foreach ( $attachments as $attachment_id => $attachment_array ) {
-				$use_standard_source = self::use_standard_source( $attachment_id );
+				$use_standard_source = Standard_Source::use_standard_source( $attachment_id );
 				$source              = self::get_image_source_text( $attachment_id );
 
 				// check if source of own images can be displayed
@@ -633,17 +636,17 @@ class ISC_Public extends ISC_Class {
 
 		foreach ( $attachments as $_attachment ) {
 			$connected_atts[ $_attachment->ID ]['source']   = self::get_image_source_text( $_attachment->ID );
-			$connected_atts[ $_attachment->ID ]['standard'] = self::use_standard_source( $_attachment->ID );
+			$connected_atts[ $_attachment->ID ]['standard'] = Standard_Source::use_standard_source( $_attachment->ID );
 			// jump to next element if the standard source is set to be excluded from the source list
-			if ( $this->is_standard_source( 'exclude' ) && '' != $connected_atts[ $_attachment->ID ]['standard'] ) {
+			if ( Standard_Source::standard_source_is( 'exclude' ) && '' != $connected_atts[ $_attachment->ID ]['standard'] ) {
 				unset( $connected_atts[ $_attachment->ID ] );
 				continue;
 			}
 
 			$connected_atts[ $_attachment->ID ]['title']       = $_attachment->post_title;
 			$connected_atts[ $_attachment->ID ]['author_name'] = '';
-			if ( $this->is_standard_source( 'custom_text' ) && ! empty( $connected_atts[ $_attachment->ID ]['own'] ) ) {
-				$connected_atts[ $_attachment->ID ]['author_name'] = $this->get_standard_source_text();
+			if ( Standard_Source::standard_source_is( 'custom_text' ) && ! empty( $connected_atts[ $_attachment->ID ]['own'] ) ) {
+				$connected_atts[ $_attachment->ID ]['author_name'] = Standard_Source::get_standard_source_text();
 			} else {
 				// show author name
 				$connected_atts[ $_attachment->ID ]['author_name'] = get_the_author_meta( 'display_name', $_attachment->post_author );
@@ -906,10 +909,8 @@ class ISC_Public extends ISC_Class {
 			$thumb = get_post( $post_id );
 
 			// don’t show caption for own image if admin choose not to do so
-			if ( $this->is_standard_source( 'exclude' ) ) {
-				if ( self::use_standard_source( $id ) ) {
-					return '';
-				}
+			if ( Standard_Source::hide_standard_source_for_image( $id ) ) {
+				return '';
 			}
 			// don’t display empty sources
 			$src           = $thumb->guid;
@@ -966,7 +967,7 @@ class ISC_Public extends ISC_Class {
 	 * @param int|string $id   id of the image.
 	 * @param string[]   $data metadata.
 	 * @param array      $args arguments
-	 *                         use "disable-links" = (any value)), to disable any working links.
+	 *                         use "disable-links" = (any value), to disable any working links.
 	 *
 	 * @return bool|string false if no source was given, else string with source
 	 */
@@ -975,27 +976,19 @@ class ISC_Public extends ISC_Class {
 		$options = $this->get_isc_options();
 
 		$metadata['source']     = $data['source'] ?? self::get_image_source_text( $id );
-		$metadata['own']        = $data['own'] ?? self::use_standard_source( $id );
-		$metadata['licence']    = $data['licence'] ?? get_post_meta( $id, 'isc_image_licence', true );
+		$metadata['own']        = $data['own'] ?? Standard_Source::use_standard_source( $id );
+		$metadata['licence']    = $data['licence'] ?? self::get_image_license( $id );
 
 		if ( ! isset( $args[ 'disable-links'] ) ) {
-			$metadata['source_url'] = $data['source_url'] ?? get_post_meta( $id, 'isc_image_source_url', true );
+			$metadata['source_url'] = $data['source_url'] ?? self::get_image_source_url( $id );
 		} else {
 			$metadata['source_url'] = '';
 		}
 
 		$source = '';
 
-		$att_post = get_post( $id );
-
 		if ( '' != $metadata['own'] ) {
-			if ( $this->is_standard_source( 'author_name' ) ) {
-				if ( ! empty( $att_post ) ) {
-					$source = get_the_author_meta( 'display_name', $att_post->post_author );
-				}
-			} else {
-				$source = $this->get_standard_source_text();
-			}
+			$source = Standard_Source::get_standard_source_text_for_attachment( $id );
 		} else {
 			if ( '' != $metadata['source'] ) {
 				$source = $metadata['source'];
@@ -1054,7 +1047,7 @@ class ISC_Public extends ISC_Class {
 		}
 
 		// don’t render the caption for own images if the admin choose not to do so
-		if ( self::is_standard_source( 'exclude' ) && ISC_Public::use_standard_source( $id ) ) {
+		if ( Standard_Source::hide_standard_source_for_image( $id ) ) {
 			ISC_Log::log( sprintf( 'render_caption_string() skipped overlay for "own" image ID "%s"', $id ) );
 			return '';
 		}
@@ -1085,20 +1078,6 @@ class ISC_Public extends ISC_Class {
 		}
 
 		return false;
-	}
-
-	/**
-	 * Check if the given attachment ought to use the standard source
-	 *
-	 * @param int $attachment_id attachment ID
-	 * @return bool true if standard source is used
-	 */
-	public static function use_standard_source( $attachment_id ) {
-		return (bool) apply_filters(
-			'isc_public_attachment_use_standard_source',
-			get_post_meta( $attachment_id, 'isc_image_source_own', true ),
-			$attachment_id
-		);
 	}
 
 	/**

commit d43a29d485dcae2f92bb828446d15383d6e6bdd1

Fix saving custom text
As a regression in 436ba99676426ca23b86cce7d4bba0f3f5e43e05, the text for custom standard sources was not saved anymore. This commit fixes that.

admin/admin.php
@@ -933,7 +933,7 @@ class ISC_Admin extends ISC_Class {
 		if ( isset( $input['by_author_text'] ) ) {
 			$output['standard_source_text'] = esc_html( $input['by_author_text'] );
 		} else {
-			$output['standard_source_text'] = ISC\Includes\Standard_Source::get_standard_source_text();
+			$output['standard_source_text'] = isset( $input['standard_source_text'] ) ? esc_attr( $input['standard_source_text'] ) : ISC\Includes\Standard_Source::get_standard_source_text();
 		}
 
 		/**