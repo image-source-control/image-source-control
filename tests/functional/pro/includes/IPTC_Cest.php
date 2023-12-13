a8cf807545fbc17c82cdd87c0c914b73198903ed
Thomas Maier <post@webzunft.de>
2023-12-13 14:42:12 +0100

    Updated functional tests
    The functional tests for IPTC standard data was a bit off after making Credit the default IPTC tag to show as a standard source.

--- tests/functional/pro/includes/IPTC_Cest.php
+++ tests/functional/pro/includes/IPTC_Cest.php
@@ -32,6 +32,31 @@ class IPTC_Cest {
 	 * - image has no source
 	 * - image is set to use the standard source
 	 * - IPTC copyright is "IPTC Copyright" and credit is "IPTC Credit"
+	 * => show the IPTC credit information: "IPTC Credit"
+	 *
+	 * @param \FunctionalTester $I
+	 *
+	 * @return void
+	 */
+	public function test_image_standard_source_shows_iptc_credit( \FunctionalTester $I ) {
+		// insert image meta information with copyright and credit fields
+		$I->havePostmetaInDatabase( 123, '_wp_attachment_metadata', [ 'image_meta' => [ 'credit' => 'IPTC Credit', 'copyright' => 'IPTC Copyright' ] ] );
+		$I->havePageInDatabase( [
+			'post_name'    => 'test-page',
+			'post_content' => '<img src="https://example.com/image-one.jpg" />',
+		] );
+
+		// Go to the page.
+		$I->amOnPage( '/test-page' );
+		$I->seeInSource( '<span id="isc_attachment_123" class="isc-source "><img decoding="async" src="https://example.com/image-one.jpg" /><span class="isc-source-text">Quelle: IPTC Credit</span></span>' );
+	}
+
+	/**
+	 * Setup:
+	 * - image has no source
+	 * - image is set to use the standard source
+	 * - IPTC copyright is "IPTC Copyright" and credit is "IPTC Credit"
+	 * - Copyright is selected as the IPTC tag to show instead of Credit
 	 * => show the IPTC copyright information: "IPTC Copyright"
 	 *
 	 * @param \FunctionalTester $I
@@ -39,12 +64,16 @@ class IPTC_Cest {
 	 * @return void
 	 */
 	public function test_image_standard_source_shows_iptc_copyright( \FunctionalTester $I ) {
-		// insert image meta information with copyright and credit fields
+		// select "copyright" as the IPTC tag to show as standard source
+		$existingOption = $I->grabOptionFromDatabase('isc_options');
+		$existingOption['standard_source_iptc_tag'] = ['copyright'];
+		$I->haveOptionInDatabase( 'isc_options', $existingOption );
+
 		$I->havePostmetaInDatabase( 123, '_wp_attachment_metadata', [ 'image_meta' => [ 'credit' => 'IPTC Credit', 'copyright' => 'IPTC Copyright' ] ] );
 		$I->havePageInDatabase( [
-			'post_name'    => 'test-page',
-			'post_content' => '<img src="https://example.com/image-one.jpg" />',
-		] );
+			                        'post_name'    => 'test-page',
+			                        'post_content' => '<img src="https://example.com/image-one.jpg" />',
+		                        ] );
 
 		// Go to the page.
 		$I->amOnPage( '/test-page' );
@@ -55,16 +84,16 @@ class IPTC_Cest {
 	 * Setup:
 	 * - image has no source
 	 * - image is set to use the standard source
-	 * - IPTC credit is "IPTC Credit", copyright is not set
-	 * => show the IPTC credit information: "IPTC Credit"
+	 * - IPTC copyright is "IPTC Copyright", credit is not set
+	 * => show the IPTC copyright information: "IPTC Copyright"
 	 *
 	 * @param \FunctionalTester $I
 	 *
 	 * @return void
 	 */
-	public function test_image_standard_source_shows_iptc_credit( \FunctionalTester $I ) {
+	public function test_image_standard_source_shows_iptc_copyright_fallback( \FunctionalTester $I ) {
 		// insert image meta information with credit field
-		$I->havePostmetaInDatabase( 123, '_wp_attachment_metadata', [ 'image_meta' => [ 'credit' => 'IPTC Credit' ] ] );
+		$I->havePostmetaInDatabase( 123, '_wp_attachment_metadata', [ 'image_meta' => [ 'copyright' => 'IPTC Copyright' ] ] );
 		$I->havePageInDatabase( [
 			                        'post_name'    => 'test-page',
 			                        'post_content' => '<img src="https://example.com/image-one.jpg" />',
@@ -72,7 +101,7 @@ class IPTC_Cest {
 
 		// Go to the page.
 		$I->amOnPage( '/test-page' );
-		$I->seeInSource( '<span id="isc_attachment_123" class="isc-source "><img decoding="async" src="https://example.com/image-one.jpg" /><span class="isc-source-text">Quelle: IPTC Credit</span></span>' );
+		$I->seeInSource( '<span id="isc_attachment_123" class="isc-source "><img decoding="async" src="https://example.com/image-one.jpg" /><span class="isc-source-text">Quelle: IPTC Copyright</span></span>' );
 	}
 
 	/**
@@ -80,9 +109,9 @@ class IPTC_Cest {
 	 * - image has no source
 	 * - image is NOT set to use the standard source
 	 * - IPTC copyright is "IPTC Copyright" and credit is "IPTC Credit"
-	 *  => show the IPTC copyright information: "IPTC Copyright"
+	 *  => show the IPTC credit information: "IPTC Credit"
 	 */
-	public function test_empty_image_source_shows_iptc_copyright( \FunctionalTester $I ) {
+	public function test_empty_image_source_shows_iptc_credit( \FunctionalTester $I ) {
 		// insert image meta information with copyright and credit fields
 		$I->havePostmetaInDatabase( 123, '_wp_attachment_metadata', [ 'image_meta' => [ 'credit' => 'IPTC Credit', 'copyright' => 'IPTC Copyright' ] ] );
 
@@ -94,6 +123,6 @@ class IPTC_Cest {
 		// Go to the page.
 		$I->amOnPage( '/test-page' );
 		// the actual HTML output, including the data attribute and the overlay code at the beginning of the DIV container
-		$I->seeInSource( '<span id="isc_attachment_123" class="isc-source "><img decoding="async" src="https://example.com/image-one.jpg" /><span class="isc-source-text">Quelle: IPTC Copyright</span></span>' );
+		$I->seeInSource( '<span id="isc_attachment_123" class="isc-source "><img decoding="async" src="https://example.com/image-one.jpg" /><span class="isc-source-text">Quelle: IPTC Credit</span></span>' );
 	}
 }
