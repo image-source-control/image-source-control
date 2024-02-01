0bae05fd579be8e6dce7230e713b51e3bbeda527
Thomas Maier <post@webzunft.de>
2024-02-01 15:05:38 +0100

    Test Unused_Images::get_unused_attachments
    Added tests for `ISC\Pro\Unused_Attachments::get_unused_attachments` and `ISC\Pro\Unused_Images::get_uploaded_to_post()`

---
+++ tests/wpunit/pro/admin/includes/Unused_Images_Get_Unused_Attachments_Test.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ISC\Tests\WPUnit;
+use \ISC\Pro\Unused_Images;
+
+include_once dirname( __FILE__, 4 ) . '/includes/Unused_Images_Test.php';
+
+/**
+ * Testing \ISC\Pro\Unused_Images::get_unused_attachments()
+ */
+class Unused_Images_Get_Unused_Attachments_Test extends \ISC\Tests\WPUnit\Unused_Images_Test {
+
+	/**
+	 * Test with the filter set to "unchecked"
+	 */
+	public function test_filter_unchecked() {
+		parent::setUpAttachments();
+
+		// get the attachments without a filter
+		$unused_attachments = Unused_Images::get_unused_attachments();
+
+		// check if the attachments are correct
+		$this->assertCount( 3, $unused_attachments );
+
+		// setting the post meta data means that the image was already checked
+		add_post_meta( $this->attachment_ids[0], 'isc_possible_usages', '', true );
+
+		$unused_attachments = Unused_Images::get_unused_attachments( [ 'filter' => 'unchecked' ] );
+
+		// check if the attachments are correct
+		$this->assertCount( 2, $unused_attachments );
+
+		delete_post_meta( $this->attachment_ids[0], 'isc_possible_usages' );
+	}
+
+	/**
+	 * Test with the filter set to "unused"
+	 */
+	public function test_filter_unused() {
+		parent::setUpAttachments();
+
+		// get the attachments without a filter
+		$unused_attachments = Unused_Images::get_unused_attachments();
+
+		// check if the attachments are correct
+		$this->assertCount( 3, $unused_attachments );
+
+		add_post_meta( $this->attachment_ids[0], 'isc_possible_usages', [], true );
+
+		$unused_attachments = Unused_Images::get_unused_attachments( [ 'filter' => 'unused' ] );
+
+		// only one attachment should match
+		$this->assertCount( 1, $unused_attachments );
+	}
+}
