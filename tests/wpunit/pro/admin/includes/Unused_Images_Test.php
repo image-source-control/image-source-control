388a7e175ceab4898365402669d2afcbb10d4a8c
Thomas Maier <post@webzunft.de>
2024-02-01 15:05:38 +0100

    Test Unused_Images::get_unused_attachments
    Added tests for `ISC\Pro\Unused_Attachments::get_unused_attachments` and `ISC\Pro\Unused_Images::get_uploaded_to_post()`

--- tests/wpunit/pro/admin/includes/Unused_Images_Test.php
+++ tests/wpunit/pro/admin/includes/Unused_Images_Test.php
@@ -8,21 +8,34 @@ use \ISC\Pro\Unused_Images;
 
 class Unused_Images_Test extends \Codeception\TestCase\WPTestCase {
 
+	/**
+	 * Image IDs
+	 * @var array
+	 */
+	protected $image_ids = [];
+
+	/**
+	 * Post IDs
+	 * @var array
+	 */
+	protected $post_ids = [];
+
 	public function setUp(): void {
 		parent::setUp();
 
-		// image 1 is part of a post content
-		$image_one_id = $this->factory->post->create( [
-			'post_title' => 'Image One',
-			'post_type'  => 'attachment',
-			'guid'       => 'https://example.com/image-one.jpg',
-		] );
-
-		$post_one_id = $this->factory->post->create( [
+		$this->post_ids['one'] = $this->factory->post->create( [
 			'post_title'   => 'Post with Image One',
 			'post_type'    => 'post',
 			'guid'         => 'https://example.com/post-with-image-one',
 			'post_content' => 'There is some text around the image <img src="https://example.com/image-one.jpg"/> which is hopefully not removed.',
+		] );
+
+		// image 1 was uploaded to post 1, hence the post_parent is set
+		$this->image_ids['one'] = $this->factory->post->create( [
+			'post_title' => 'Image One',
+			'post_type'  => 'attachment',
+			'guid'       => 'https://example.com/image-one.jpg',
+			'post_parent' => $this->post_ids['one'],
 		] );
 
 		// image 2 is used as a featured image
@@ -118,5 +131,15 @@ class Unused_Images_Test extends \Codeception\TestCase\WPTestCase {
 		// check the actual returned post
 		$actual_object = $result[0];
 		$this->assertEquals( 'some_temporary_option', $actual_object->option_name );
+	}
+
+	/**
+	 * Test the get_uploaded_to_post() function to see if it returns the correct post Object.
+	 */
+	public function test_uploaded_to_post() {
+		$result = Unused_Images::get_uploaded_to_post( $this->image_ids['one'] );
+
+		// check the actual returned post
+		$this->assertEquals( 'Post with Image One', $result->post_title );
 	}
 }
