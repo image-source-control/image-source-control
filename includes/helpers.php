af57ac33142aaa64252996a0bb2f8a094355a565
Thomas Maier <post@webzunft.de>
2024-02-09 11:14:32 +0100

    Custom maybe_unserialize() function
    With even WordPress core functions to unserialize options being unsafe if going through unknown data, I introduced a new function that is a copy of the core `maybe_unserialize()` with an additional flag to not allow classes.
    
    Replaced the only usage of core’s `maybe_unserialize` with the new helper function.

--- includes/helpers.php
+++ includes/helpers.php
@@ -26,4 +26,22 @@ class Helpers {
 
 		return false;
 	}
+
+	/**
+	 * Securely unserialize a WordPress option
+	 * a copy of WP core’s maybe_unserialize() function with additional code to prevent object injection
+	 * See https://www.tenable.com/security/research/tra-2023-7 about the risks of object injections
+	 *
+	 * @param string $data value to unserialize.
+	 *
+	 * @return mixed Unserialized data can be any type.
+	 */
+	public static function maybe_unserialize( $data ) {
+		if ( is_serialized( $data ) ) {
+			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.serialize_unserialize, WordPress.PHP.NoSilencedErrors.Discouraged -- unserialize() is the only way to unserialize WP options
+			return @unserialize( trim( $data ), [ 'allowed_classes' => false ] );
+		}
+
+		return $data;
+	}
 }
