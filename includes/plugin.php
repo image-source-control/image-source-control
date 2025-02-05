e14450918f93b9c83cafafbcf1043c68bfe10eb8
Thomas Maier <post@webzunft.de>
2025-02-05 17:49:55 +0100

    Add module management and settings for plugin options

--- includes/plugin.php
+++ includes/plugin.php
@@ -23,4 +23,22 @@ class Plugin {
 	public static function get_options() {
 		return Options::get_options();
 	}
+
+	/**
+	 * Return true if the mentioned module is enabled
+	 *
+	 * @param string $module module name.
+	 *
+	 * @return bool
+	 */
+	public static function is_module_enabled( string $module ) {
+		$options = Options::get_options();
+
+		// all modules are enabled by default; i.e., when the option is not set
+		if ( ! isset( $options['modules'] ) ) {
+			return true;
+		}
+
+		return ( is_array( $options['modules'] ) && in_array( $module, $options['modules'], true ) );
+	}
 }
