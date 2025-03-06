1430bd1735920129088005f3c4fdb2451328cab2
Thomas Maier <post@webzunft.de>
2025-03-06 16:38:41 +0100

    Added image URL extract methods
    Added `Analyze_HTML:extract_image_urls_from_html_tags()` and `Analyze_HTML:extract_image_urls()` for more use cases.

--- includes/image-sources/analyze-html.php
+++ includes/image-sources/analyze-html.php
@@ -2,6 +2,8 @@
 
 namespace ISC\Image_Sources;
 
+use ISC_Log;
+
 /**
  * Analyze chunks of HTML
  */
@@ -13,7 +15,7 @@ class Analyze_HTML {
 	 * @param string $html HTML to extract images from.
 	 * @return array Array of image markup.
 	 */
-	public function extract_images_from_html( string $html ) : array {
+	public function extract_images_from_html( string $html ): array {
 		/**
 		 * Removed [caption], because this check runs after the hook that interprets shortcodes
 		 * img tag is checked individually since there is a different order of attributes when images are used in gallery or individually
@@ -45,7 +47,7 @@ class Analyze_HTML {
 		 * Convert the matches into a multidimensional array with string keys to simplify usage and manipulation
 		 */
 		$matches_new = array_map(
-			function( $match ) {
+			function ( $match ) {
 				return [
 					'full'         => $match[0] ?? '',
 					'figure_class' => $match[1] ?? '',
@@ -68,7 +70,7 @@ class Analyze_HTML {
 	 * @param string $html HTML to extract an image ID from.
 	 * @return int Image ID.
 	 */
-	public function extract_image_id( string $html ) : int {
+	public function extract_image_id( string $html ): int {
 		if ( ! $html ) {
 			return 0;
 		}
@@ -83,9 +85,9 @@ class Analyze_HTML {
 		$success = preg_match( '#wp-image-(\d+)|data-id="(\d+)#is', $html, $matches_id );
 		if ( $success ) {
 			$id = $matches_id[1] ? intval( $matches_id[1] ) : intval( $matches_id[2] );
-			\ISC_Log::log( sprintf( 'found ID "%s"', $id ) );
+			ISC_Log::log( sprintf( 'found ID "%s"', $id ) );
 		} else {
-			\ISC_Log::log( sprintf( 'no ID found for "%s"', $html ) );
+			ISC_Log::log( sprintf( 'no ID found for "%s"', $html ) );
 		}
 
 		return $id;
@@ -97,7 +99,7 @@ class Analyze_HTML {
 	 * @param string $html HTML to extract an image URL from.
 	 * @return string Image URL.
 	 */
-	public function extract_image_src( string $html ) : string {
+	public function extract_image_src( string $html ): string {
 		if ( ! $html ) {
 			return '';
 		}
@@ -107,11 +109,104 @@ class Analyze_HTML {
 		$success = preg_match( '#src="([^"]+)"#is', $html, $matches_src );
 		if ( $success ) {
 			$src = $matches_src[1];
-			\ISC_Log::log( sprintf( 'found src "%s"', $src ) );
+			ISC_Log::log( sprintf( 'found src "%s"', $src ) );
 		} else {
-			\ISC_Log::log( sprintf( 'no src found for "%s"', $html ) );
+			ISC_Log::log( sprintf( 'no src found for "%s"', $html ) );
 		}
 
 		return $src;
+	}
+
+	/**
+	 * Extract image URLs from HTML.
+	 * One image URL per HTML tag will be found.
+	 *
+	 * @param string $html Any HTML code.
+	 * @return array List of image URLs.
+	 */
+	public static function extract_image_urls_from_html_tags( $html = '' ): array {
+		$urls = [];
+
+		if ( empty( $html ) ) {
+			ISC_Log::log( 'Exit due to missing empty HTML' );
+			return $urls;
+		}
+
+		ISC_Log::log( 'look for image IDs within the HTML' );
+
+		$types = implode( '|', \ISC\Image_Sources\Image_Sources::get_instance()->allowed_extensions );
+		/**
+		 * Look for any URLs
+		 * - starting with http, or https
+		 * - ending with a valid image format or " or '
+		 * - or any URL in the SRG attribute of IMG tags, where we ignore the extension
+		 *
+		 * What it does
+		 * (?<!, ) – needed to prevent multiple images from `srcset` attribute to being considered here
+		 * (http[s]?:) - start with http or https
+		 * [^\'^"^ ] - stop at one of these chars since they don't belong into URLs: ", ', or space
+		 * or looks for any URL in the SRC attribute
+		 *
+		 * Limitations
+		 * - we don't include images that don't have a full path, e.g., url("../img/image.png") or url("image.png"); they are likely not in the Media library
+		 */
+		$pattern = '#((?<!, )((http[s]?:)[^\'^"^ ]*\.(' . $types . ')))|(<img[\x20|\x9|\xD|\xA]+[^>]*[^>]*src="(.+)".*\/?>)#isU';
+
+		/**
+		 * Match index
+		 * 0 - full match (including first char)
+		 * 1 - URLs with valid image file extension
+		 * 2 - ignore
+		 * 3 – protocol
+		 * 4 - file format (e.g. "jpg")
+		 * 5 – full img tag
+		 * 6 - URLs from img tags – the file extension is ignored here
+		 */
+		preg_match_all( $pattern, $html, $matches );
+
+		// merge matches from both conditions and remove empty ones
+		$urls = array_filter( array_merge( $matches[1], $matches[6] ) );
+		// remove duplicate URLs
+		return array_unique( $urls );
+	}
+
+	/**
+	 * Extract any image URLs from HTML.
+	 *
+	 * @param string $html Any HTML code.
+	 *
+	 * @return array List of image URLs.
+	 */
+	public static function extract_image_urls( string $html = '' ): array {
+		$urls = [];
+
+		if ( empty( $html ) ) {
+			\ISC_Log::log( 'Exit due to empty HTML' );
+			return $urls;
+		}
+
+		\ISC_Log::log( 'Looking for valid image URLs within HTML' );
+
+		// Get allowed image extensions as a pipe-separated string.
+		$types = implode( '|', \ISC\Image_Sources\Image_Sources::get_instance()->allowed_extensions );
+
+		/**
+		 * The regex below matches:
+		 * - URLs starting with "http://" or "https://"
+		 * - Followed by one or more non-whitespace characters (non-greedy)
+		 * - Ending with a dot and one of the allowed extensions (case-insensitive)
+		 * - Optionally followed by a query string
+		 * - The URL is expected to end before a closing quote or whitespace.
+		 */
+		$pattern = '#https?://\S+?\.(?:' . $types . ')(?:\?\S*)?(?=["\'\s])#i';
+
+		preg_match_all( $pattern, $html, $matches );
+
+		if ( ! empty( $matches[0] ) ) {
+			// Remove duplicate URLs.
+			$urls = array_unique( $matches[0] );
+		}
+
+		return $urls;
 	}
 }
