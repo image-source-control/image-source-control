5bcea31c6921e26bba6b4bfacabb7e77b0ff2f7d
Thomas Maier <post@webzunft.de>
2025-04-23 21:19:21 +0200

    Fix test script
    Fixed script for testing for various attributes

--- scripts/php-tests.sh
+++ scripts/php-tests.sh
@@ -20,7 +20,7 @@ is_version_token() {
   || [[ "$1" =~ ^[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]
 }
 
-for tok in "${raw_args[@]}"; do
+for tok in "${raw_args[@]:-}"; do
   if is_version_token "$tok"; then
     if [[ "$tok" == "phpall" ]]; then
       VERSIONS_TO_RUN=("${PHP_VERSIONS[@]}")
@@ -55,6 +55,20 @@ for i in "${!VERSIONS_TO_RUN[@]}"; do
   VERSIONS_TO_RUN[$i]="$(resolve_version "${VERSIONS_TO_RUN[$i]}")"
 done
 
+# Remove duplicates and sort the versions to run
+if ((${#VERSIONS_TO_RUN[@]})); then
+    sorted_unique_versions_output=$(printf "%s\n" "${VERSIONS_TO_RUN[@]}" | sort -u)
+
+    # Clear the original array
+    VERSIONS_TO_RUN=()
+
+    # Read the sorted unique versions back into the array line by line
+    while IFS= read -r line; do
+        VERSIONS_TO_RUN+=("$line")
+    done <<< "$sorted_unique_versions_output"
+fi
+
+
 ###################
 # Run the suites  #
 ###################
@@ -65,12 +79,15 @@ run_suite () {
   printf "\n▶︎  %s  •  PHP %s\n" "$suite" "$version"
   export ISC_EXPECT_PHP_VERSION="$version"
 
-  "$bin" vendor/bin/codecept run "$suite" "${CODECEPT_ARGS[@]}"
+  # GEÄNDERT: Sicherer Zugriff auf CODECEPT_ARGS
+  "$bin" vendor/bin/codecept run "$suite" "${CODECEPT_ARGS[@]:-}"
 
   unset ISC_EXPECT_PHP_VERSION
 }
 
+# Determine which PHP versions to use
 if ((${#VERSIONS_TO_RUN[@]})); then
+  echo "Running suite '${suite}' across specified PHP versions: ${VERSIONS_TO_RUN[*]}..."
   for v in "${VERSIONS_TO_RUN[@]}"; do
     bin="${MAMP_ROOT}/php${v}/bin/php"
     run_suite "$bin" "$v"
@@ -78,5 +95,6 @@ if ((${#VERSIONS_TO_RUN[@]})); then
 else
   current_php="${PHP_BINARY:-php}"
   current_version="$($current_php -r 'echo PHP_VERSION;')"
+  echo "Running suite '${suite}' with current PHP CLI version ($current_version)..."
   run_suite "$current_php" "$current_version"
 fi
