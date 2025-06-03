4922271d5188cc57a462c323b8b888fa842fe383
Thomas Maier <post@webzunft.de>
2025-06-03 09:06:25 +0200

    Test for Caption styles updates

--- tests/wpunit/Pblc/Captions/Caption_Style_Option_Test.php
+++ tests/wpunit/Pblc/Captions/Caption_Style_Option_Test.php
@@ -23,6 +23,8 @@ class Caption_Style_Option_Test extends WPTestCase {
 		// Reset the wp_scripts global
 		global $wp_scripts;
 		$wp_scripts = null;
+
+		set_current_screen( 'front' );
 	}
 
 	/**
@@ -30,7 +32,7 @@ class Caption_Style_Option_Test extends WPTestCase {
 	 *
 	 * @dataProvider captionStyleProvider
 	 */
-	public function test_script_and_head_output_based_on_caption_style( $caption_style, $should_enqueue, $expected_in_head ) {
+	public function test_script_and_head_output_based_on_caption_style( $caption_style, $should_enqueue, $expected_data_and_styles ) {
 		global $wp_scripts;
 
 		$this->setCaptionStyle($caption_style);
@@ -42,10 +44,21 @@ class Caption_Style_Option_Test extends WPTestCase {
 			$this->assertNotContains( 'isc_caption', $wp_scripts->queue, 'Script should not be enqueued' );
 		}
 
-		if ( $expected_in_head ) {
-			$this->assertStringContainsString( 'var isc_front_data', $head_content, 'Frontend data should be printed' );
+		$localized_data_string = $wp_scripts->get_data( 'isc_caption', 'data' );
+		if ( $expected_data_and_styles ) {
+			$this->assertNotEmpty( $localized_data_string, 'Localized data string should exist for "isc_caption".' );
+			$this->assertStringContainsString( 'var isc_front_data =', $localized_data_string, 'Localized data string should define "isc_front_data".' );
+			$this->assertStringContainsString( 'caption_position', $localized_data_string, '"caption_position" should be in localized data.' );
+
+			$this->assertStringNotContainsString( 'var isc_front_data', $head_content, '"isc_front_data" should NOT be printed directly in wp_head anymore.' );
+
+			$this->assertStringContainsString( '.isc-source {', $head_content, 'Caption CSS styles should be in head when captions are active.' );
 		} else {
-			$this->assertStringNotContainsString( 'var isc_front_data', $head_content, 'Frontend data should not be printed' );
+			$this->assertEmpty( $localized_data_string, 'Localized data string should NOT exist for "isc_caption" when captions are inactive.' );
+
+			$this->assertStringNotContainsString( 'var isc_front_data', $head_content, '"isc_front_data" should not be printed in wp_head.' );
+
+			$this->assertStringNotContainsString( '.isc-source {', $head_content, 'Caption CSS styles should NOT be in head when captions are inactive.' );
 		}
 	}
 
@@ -81,6 +94,11 @@ class Caption_Style_Option_Test extends WPTestCase {
 		// Reset the wp_scripts global
 		global $wp_scripts;
 		$wp_scripts = null;
+
+		remove_all_actions( 'wp' );
+		remove_all_actions( 'wp_enqueue_scripts' );
+		remove_all_actions( 'wp_head' );
+
 		parent::tearDown();
 	}
 }
