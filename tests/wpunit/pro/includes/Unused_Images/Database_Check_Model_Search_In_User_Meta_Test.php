53e9e74b68c9f66adcdbaeb4bf988c46e165e99c
Thomas Maier <post@webzunft.de>
2025-07-15 10:15:38 +0200

    Test database search in user meta
    Test the new `Database_Check_Model::search_filepath_in_user_meta()` method in Pro

---
+++ tests/wpunit/pro/includes/Unused_Images/Database_Check_Model_Search_In_User_Meta_Test.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ISC\Tests\WPUnit\Pro\Includes\Unused_Images;
+
+use ISC\Tests\WPUnit\WPTestCase;
+use ISC\Pro\Unused_Images\Database_Check_Model;
+
+class Database_Check_Model_Search_In_User_Meta_Test extends WPTestCase {
+
+	/**
+	 * User ID for testing
+	 * @var int
+	 */
+	protected $user_id;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->user_id = $this->factory->user->create( [
+			                                         'user_email' => 'user@example.com',
+			                                         'user_login' => 'user1',
+		                                         ] );
+	}
+
+	/**
+	 * Test the search_filepath_in_user_meta() function to see if it returns the only image within user meta.
+	 */
+	public function test_file_path_in_user_meta() {
+		update_user_meta( $this->user_id, 'profile_picture', 'https://example.com/image-four.png' );
+
+		$result = ( new Database_Check_Model() )->search_filepath_in_user_meta( 'image-four' );
+
+		$this->assertCount( 1, $result );
+		$actual_object = $result[0];
+		$this->assertEquals( 'profile_picture', $actual_object->meta_key );
+		$this->assertEquals( $this->user_id, $actual_object->user_id );
+	}
+
+	/**
+	 * Test the search_filepath_in_user_meta() function returns no result for an unmatched string.
+	 */
+	public function test_file_path_in_user_meta_no_result() {
+		update_user_meta( $this->user_id, 'profile_picture', 'https://example.com/image-xyz.png' );
+
+		$result = ( new Database_Check_Model() )->search_filepath_in_user_meta( 'image-four' );
+
+		$this->assertCount( 0, $result );
+	}
+}
