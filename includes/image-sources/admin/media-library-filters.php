744c2779a15ba70805c68139141b09aca09ab9e5
Thomas Maier <post@webzunft.de>
2025-09-10 10:25:38 +0200

    Fix Images-Only option applied to media library
    The Images-only option caused non-images to disappear entirely from the media library. The fix now only filters out non-images if another ISC-filter for image sources was set.

--- includes/image-sources/admin/media-library-filters.php
+++ includes/image-sources/admin/media-library-filters.php
@@ -56,8 +56,8 @@ class Admin_Media_Library_Filters {
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		$filter = isset( $_GET['isc_filter'] ) ? sanitize_text_field( wp_unslash( $_GET['isc_filter'] ) ) : '';
 
-		// Add image type check if images_only is enabled
-		if ( \ISC\Media_Type_Checker::enabled_images_only_option() ) {
+		// Add image type check if images_only is enabled and a filter for image sources is checked
+		if ( $filter && \ISC\Media_Type_Checker::enabled_images_only_option() ) {
 			$query->set( 'post_mime_type', 'image%' );
 		}
 
